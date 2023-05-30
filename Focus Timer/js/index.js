3dfca02566dd1e3c30f838ff95bdbe1a0b0ef0dc
Antonio Pedro <122944610+antoniopbraga@users.noreply.github.com>
2023-05-29 21:39:01 -0300

    Add files via upload

---
+++ Focus Timer/js/index.js
@@ -0,0 +1,20 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {buttonPlay, buttonPause, buttonStop, buttonSet, displayMinutes, displaySeconds} from "./elements.js"
+
+const controls = Controls( {
+  buttonPause, 
+  buttonPlay, 
+  buttonSet, 
+  buttonStop } )
+
+const timer = Timer( {
+  displayMinutes,
+  displaySeconds,
+  resetControls: controls.reset } )
+
+const sound = Sound()
+
+Events({controls, timer, sound})
